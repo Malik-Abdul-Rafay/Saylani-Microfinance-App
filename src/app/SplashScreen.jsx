45d4b7c5ecfed4b0dfb03b0822f8ba33fa199bc1
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-02-13 19:07:01 +0500

    hay

--- src/app/SplashScreen.jsx
+++ src/app/SplashScreen.jsx
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet, StatusBar, Image } from 'react-native'
 import React, { useEffect } from 'react' 
 import { useNavigation } from '@react-navigation/native'    
-import img from '../../assets/images/logo.png'   
+import img from '../../assets/images/logo.png'    
     
 const SplashScreen = () => {
   const navigation = useNavigation()
