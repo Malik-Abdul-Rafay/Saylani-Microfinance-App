34215642157b7a2f9596bd6a5b26605648cc235f
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-02-24 22:53:10 +0500

    sjsj

--- src/app/AppNavigation.jsx
+++ src/app/AppNavigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';    
 import { createStackNavigator } from '@react-navigation/stack';                  
-import SplashScreen from './SplashScreen';   
+import SplashScreen from './SplashScreen';    
 import ParentScreen from './ParentScreen'; 
 import LoginScreen from '../screens/LoginScreen';    
 import RegisterScreen from '../screens/RegisterScreen'; 
