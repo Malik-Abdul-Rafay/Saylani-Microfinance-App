32d881b347e21805e465e0729ee37428bf4ca5e6
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-03-01 22:12:29 +0500

    shsh

--- src/app/AppNavigation.jsx
+++ src/app/AppNavigation.jsx
@@ -7,7 +7,7 @@ import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';       
        
 const Stack = createStackNavigator();  
-const AppNavigation = () => {  
+const AppNavigation = () => {   
   return (  
     <Stack.Navigator> 
       <Stack.Screen name="Splash" component={SplashScreen}  options={{ headerShown: false }} />
