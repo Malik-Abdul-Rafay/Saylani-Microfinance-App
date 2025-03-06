a5c68790051df5411f3bdebfd052437b81626945
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-03-06 23:43:07 +0500

    s

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import AppNavigation from './src/app/AppNavigation'
 import SystemNavigationBar from 'react-native-system-navigation-bar';    
- 
+  
  
    
 
