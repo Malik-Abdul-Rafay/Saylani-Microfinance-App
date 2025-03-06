4f84548753a21844c6e9b960152d10c7e44ec059
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-03-06 23:47:30 +0500

    s

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ import AppNavigation from './src/app/AppNavigation'
 import SystemNavigationBar from 'react-native-system-navigation-bar';     
      
    
-     
+      
  
 const App = () => {
   SystemNavigationBar.setNavigationColor('#0066B3'); 
