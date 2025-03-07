49aafca85441a77e984be2d112477549cadbaf22
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-03-07 21:38:49 +0500

    main

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import AppNavigation from './src/app/AppNavigation'  
+import AppNavigation from './src/app/AppNavigator'  
 import SystemNavigationBar from 'react-native-system-navigation-bar';     
       
    
