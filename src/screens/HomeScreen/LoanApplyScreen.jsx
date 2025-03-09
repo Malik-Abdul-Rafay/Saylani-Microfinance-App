d3c359687f7348622271483016a18f0de0df6290
Malik-Abdul-Rafay <malikabdulrafay290@gmail.com>
2025-03-09 23:22:12 +0500

    ss

--- src/screens/HomeScreen/LoanApplyScreen.jsx
+++ src/screens/HomeScreen/LoanApplyScreen.jsx
@@ -96,7 +96,7 @@ const LoanApplyScreen = ({ route, navigation }) => {
  
           <View style={styles.loanBox}> 
             <Text style={styles.loanText}>Your approved loan amount Up to</Text>
-            <Text style={styles.loanAmount}>RS. {loanAmount}/-</Text> 
+            <Text style={styles.loanAmount}>RS. {loanAmount}/-</Text>  
             <Slider 
               style={styles.slider}    
               minimumValue={5000}    
